55d7d68182b472eb8fe485904a796fb8ac0496c7
veronicavillegas <urseza@hotmail.com>
2012-09-27 16:27:17 -0300

    commit branch 2

--- AppExample/AppExample/Program.cs
+++ AppExample/AppExample/Program.cs
@@ -16,6 +16,7 @@ namespace AppExample
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //vereoncia comentario branch 2
         }
     }
 }
